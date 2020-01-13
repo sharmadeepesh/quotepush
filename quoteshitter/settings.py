26ed6ecd7f4f2ac9a21d7e78ed0d40bb2cccdbe2
Deepesh Sharma <49898576+sharmadeepesh@users.noreply.github.com>
2020-01-13 14:20:04 +0530

    Update settings.py

--- quoteshitter/settings.py
+++ quoteshitter/settings.py
@@ -23,7 +23,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,"templates/")
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l^zfyu-lomafx%gk!_-$!)5wlcookpj()9k(+r(38+y2*wcnk3'
+SECRET_KEY = 'ADD YOUR SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
